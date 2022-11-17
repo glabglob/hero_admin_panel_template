665c745ef4dd570998899aaa22dee06bb0f21281
Gleb <basketking57@gmail.com>
2022-11-17 06:29:25 +0200

    add Middleware

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,20 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import heroesReducer from '../reducers/heroes';
 import filtersReducer from '../reducers/filters';
 
-const store = createStore(combineReducers({ heroesReducer, filtersReducer }),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const stringMiddleWare = () => (next) => (actions) => {
+    if (typeof actions === 'string') {
+        return next({
+            type: actions
+        })
+    }
+    return next(actions);
+};
+
+const store = createStore(
+    combineReducers({ heroesReducer, filtersReducer }),
+    compose(applyMiddleware(stringMiddleWare), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
+
 
 export default store;
+
