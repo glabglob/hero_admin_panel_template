5e38351ae0430d1fd58aff245e69097df863d2d0
Gleb <basketking57@gmail.com>
2022-11-18 11:49:12 +0200

    rewrote reducers with createReducer

--- src/reducers/heroes.js
+++ src/reducers/heroes.js
@@ -1,38 +1,76 @@
+import { createReducer } from "@reduxjs/toolkit";
+
+import {
+    heroesFetching,
+    heroesFetched,
+    heroesFetchingError,
+    heroCreateByForm,
+    heroDeletedById
+} from '../actions'
+
 const initialState = {
     heroes: [],
     heroesLoadingStatus: 'idle',
 }
 
-const heroesReducer = (state = initialState, action) => {
-    switch (action.type) {
-        case 'HEROES_FETCHING':
-            return {
-                ...state,
-                heroesLoadingStatus: 'loading'
-            }
-        case 'HEROES_FETCHED':
-            return {
-                ...state,
-                heroes: action.payload,
-                heroesLoadingStatus: 'idle'
-            }
-        case 'HEROES_FETCHING_ERROR':
-            return {
-                ...state,
-                heroesLoadingStatus: 'error'
-            }
-        case 'HERO_DELETED': //(1) задание из  HeroesList
-            return {
-                ...state,
-                heroes: state.heroes.filter(item => item.id !== action.payload),
-            }
-        case 'HERO_CREATED': // (3) задание из HeroesAddForm
-            return {
-                ...state,
-                heroes: [...state.heroes, action.payload]
-            }
-        default: return state
-    }
-}
+const heroesReducer = createReducer(initialState, builder => {
+    builder
+        .addCase(heroesFetching, state => {
+            //визуально это выглядит  не иммутабельно
+            //но библиотека преравитит этот код в 
+            //код с использованием принципов иммутабельности
+            // как в строке 31 - 34
+            //в такой функции  не нужно ничего возвращать
+            state.heroesLoadingStatus = 'loading';
+        })
+        .addCase(heroesFetched, (state, action) => {
+            state.heroesLoadingStatus = 'idle';
+            state.heroes = action.payload;
+        })
+        .addCase(heroesFetchingError, state => {
+            state.heroesLoadingStatus = 'error';
+        })
+        .addCase(heroCreateByForm, (state, action) => {
+            state.heroes.push(action.payload);
+        })
+        .addCase(heroDeletedById, (state, action) => {
+            state.heroes = state.heroes.filter(item => item.id !== action.payload)
+        })
+        .addDefaultCase(()=>{});
+})
+
+// без  использования Redux Toolkit
+
+// const heroesReducer = (state = initialState, action) => {
+//     switch (action.type) {
+//         case 'HEROES_FETCHING':
+//             return {
+//                 ...state,
+//                 heroesLoadingStatus: 'loading'
+//             }
+//         case 'HEROES_FETCHED':
+//             return {
+//                 ...state,
+//                 heroes: action.payload,
+//                 heroesLoadingStatus: 'idle'
+//             }
+//         case 'HEROES_FETCHING_ERROR':
+//             return {
+//                 ...state,
+//                 heroesLoadingStatus: 'error'
+//             }
+//         case 'HERO_DELETED': //(1) задание из  HeroesList
+//             return {
+//                 ...state,
+//                 heroes: state.heroes.filter(item => item.id !== action.payload),
+//             }
+//         case 'HERO_CREATED': // (3) задание из HeroesAddForm
+//             return {
+//                 ...state,
+//                 heroes: [...state.heroes, action.payload]
+//             }
+//         default: return state
+//     }
+// }
 
 export default heroesReducer;
